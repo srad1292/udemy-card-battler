933312f9e35283d6342551a4c38077447148660d
Sam Radford <smr1292@yahoo.com>
2023-03-07 19:28:13 -0500

    Showing cards as if they are in your hand and not already in playing area

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    public List<Card> cardsInHand;
+    public Transform minPos;
+    public Transform maxPos;
+    public List<Vector3> cardPositions = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCardPositionsInHand();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCardPositionsInHand() {
+        cardPositions.Clear();
+
+        Vector3 distanceBetweenPoints = Vector3.zero;
+        if(cardsInHand.Count > 1) {
+            distanceBetweenPoints = (maxPos.position - minPos.position) / (cardsInHand.Count - 1);
+        }
+
+        for(int idx = 0; idx < cardsInHand.Count; idx++) {
+            cardPositions.Add(minPos.position + (idx*distanceBetweenPoints));
+            cardsInHand[idx].transform.position = cardPositions[idx];
+            cardsInHand[idx].transform.rotation = minPos.rotation;
+        }
+    }
+}
